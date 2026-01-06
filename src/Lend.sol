5018c81bbb6d4890ac3bf0892bc5fe07cae43628
Ujjwal  Thakur <105385470+smartcondev@users.noreply.github.com>
2026-01-06 10:21:38 +0530

    Improved the Puzzle Description

--- src/Lend.sol
+++ src/Lend.sol
@@ -1,9 +1,27 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 import { IVaultV2 } from "@morpho-org/vault-v2/src/interfaces/IVaultV2.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * PUZZLE: Lend to Morpho Vault
+ * 
+ * SCENARIO:
+ * You have 1,000 USDC in this contract.
+ * Your goal is to deposit USDC into the Morpho Vault to earn yield.
+ * 
+ * OBJECTIVE:
+ * Implement the supplyAsset() function to:
+ * 1. Approve the vault to spend USDC from this contract
+ * 2. Deposit the specified amount of USDC into the vault
+ * 3. Receive vault shares in return
+ * 
+ * SUCCESS CRITERIA:
+ * - This contract holds vault shares after execution
+ * - The vault shares represent your deposited USDC
+ */
 contract Lend {
 
     IVaultV2 public immutable vault;
@@ -15,6 +33,7 @@ contract Lend {
     /**
      * @notice After this function executes,
      * this contract should have vault shares.
+     * @param amount The amount of underlying asset to deposit
      */
     function supplyAsset(uint256 amount) external {
         // Add your code here
